cd925339f10d0031507d18f717db0bbed963050f
Sam Dicken <9944905+ProfessorDudley@users.noreply.github.com>
2026-02-02 15:34:25 +0000

    Added simple zoom control to player character.

--- actors/player_character.gd
+++ actors/player_character.gd
@@ -1,10 +1,15 @@
 class_name PlayerCharacter extends CharacterBody3D
 
 @export var _move_speed: float = 1000
+@export var _zoom_speed: float = 1
+@export var _min_zoom_distance: float = 4
+@export var _max_zoom_distance: float = 10
 
 @onready var player_stats: PlayerStats = $player_stats
 
 func _process(_delta: float):
+	
+	## Left and Right Movement
 	var _input_vector: Vector3 = Vector3.ZERO # The player's movement vector.
 	if Input.is_action_pressed(&"move_right"):
 		_input_vector.x += 1
@@ -18,3 +23,13 @@ func _process(_delta: float):
 	velocity.z = _input_vector.z * _move_speed * _delta
 	
 	move_and_slide()
+	
+	## Zoom Movement
+	var _input_zoom: float = 0.0
+	if Input.is_action_pressed(&"zoom_in") && $Camera3D.position.z > _min_zoom_distance:
+		_input_zoom -= 1
+	if Input.is_action_pressed(&"zoom_out") && $Camera3D.position.z < _max_zoom_distance:
+		_input_zoom += 1
+	
+	if _input_zoom != 0:
+		$Camera3D.position.z += _input_zoom * _zoom_speed * _delta
